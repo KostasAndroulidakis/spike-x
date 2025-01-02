bc49b1ff590b49451dd94e392c6db607fad9a696
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-01-02 14:18:06 +0200

    Added Three.js setup with rotating cube in Canvas3D component

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Canvas3D from "../components/Canvas3D";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Spike-X!</h1>
+      <p>This is the starting point for your frontend development.</p>
+      <Canvas3D />
+    </div>
+  );
+}
