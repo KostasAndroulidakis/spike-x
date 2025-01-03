b6b10a3a4d86419eaa090a47654668f1a6528ea6
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-01-03 13:21:07 +0200

    feat: Set text color to white

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1>Welcome to your new Remix app!</h1>
+      <h1 className="text-white">Welcome to Spike-X!</h1>
     </div>
   );
 }
