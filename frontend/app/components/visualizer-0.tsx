075d4095099c3e78137b4fd32663e7f8e1d5f5c9
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-01-05 00:57:50 +0200

    feat: prepare real-time visualizer, rename old test visualizer to visualizer_0

--- frontend/app/components/visualizer-0.tsx
+++ frontend/app/components/visualizer-0.tsx
@@ -2,9 +2,8 @@ import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-const Canvas3D: React.FC = () => {
+const Visualizer0: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -102,4 +101,4 @@ const Canvas3D: React.FC = () => {
   return <div ref={mountRef} />;
 };
 
-export default Canvas3D;
+export default Visualizer0;
--- frontend/app/routes/visualization.tsx
+++ frontend/app/routes/visualization.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import Canvas3D from "../components/Canvas3D";
+// spike-x/frontend/app/routes/visualization.tsx
 
-export default function Home() {
-    return (
-      <div>
-        <Canvas3D />
+import React, { useState } from "react";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
+import Visualizer from "/Users/kanon/projects/spike-x/frontend/app/components/Visualizer.tsx"; 
+
+
+const Visualization = () => {
+  const [layers, setLayers] = useState([]);
+
+  return (
+    <div className="p-4 max-w-screen-lg mx-auto">
+      <div className="mt-8">
+        <h2 className="text-xl font-semibold mb-2">Real-Time Visualization</h2>
+        <Visualizer layers={layers} />
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default Visualization;
