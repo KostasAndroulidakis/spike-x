4212aae56b5ecb6dd2bedef898c4154d8bcba47e
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-01-04 15:00:34 +0200

    fix: tailwind.css

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./routes/**/*.{js,ts,jsx,tsx}",
