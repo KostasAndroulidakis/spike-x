ae909abe27d6eaea88c91bf7c20bf99acb06f171
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-01-08 23:24:19 +0200

    feat: run cmake to setup the gprc server

---
+++ api/build/CMakeFiles/test_setup.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kanon/projects/spike-x/api/server.cpp" "CMakeFiles/test_setup.dir/server.cpp.o" "gcc" "CMakeFiles/test_setup.dir/server.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
